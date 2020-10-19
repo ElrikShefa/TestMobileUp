2b2a95937576e8ac23922fb701047ac4cd3cc723
Matvey Chernyshov <elrik.shefa@gmail.com>
2020-10-19 22:13:18 +0300

    Initial Commit

---
+++ TestMobileUp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestMobileUp
+//
+//  Created by Матвей Чернышев on 19.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
